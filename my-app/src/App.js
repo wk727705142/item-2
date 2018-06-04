f731db4ce37d079735ee9eb08b2b852b0b01b15b
wk727705142 <727705142@qq.com>
2018-06-04 14:51:33 +0800

    更新于2018-6-4

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/app.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        成功
+      </div>
+    );
+  }
+}
+
+export default App;
